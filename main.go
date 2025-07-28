01fd228c63d9526071462ead3bb86433db056685
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-07-28 22:11:29 +0700

    Use concurrency_2 module in main.go to handle additional concurrency scenario and improve error handling with logging

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "nawaphon.com/concurrency_1"
+import (
+	"log"
+	"nawaphon.com/concurrency_1"
+	"nawaphon.com/concurrency_2"
+)
 
 func main() {
-	concurrency_1.Run()
+	err := concurrency_1.Run()
 
-	concurrency_1.Run() // should error
+	if err != nil {
+		log.Fatal("I don't expected error at this point.")
+	}
+
+	err = concurrency_1.Run() // should error
+
+	if err == nil {
+		log.Printf("I expected error at this point.")
+	}
+
+	err = concurrency_2.Run()
+
+	if err != nil {
+		log.Fatal("I don't expected error at this point.")
+	}
 }
