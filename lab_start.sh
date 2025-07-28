9cb27aee474fe0906db7e804fc1b4b04f4d9319f
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-07-28 22:40:17 +0700

    Update `lab_start.sh` to execute `nawaphon.com` with concurrency, manage process IDs, and clean up `.txt` files

--- lab_start.sh
+++ lab_start.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 
-go build ./main
+rm ./*.txt
 
-CODE=$(./main)
+go build
 
-if [[ "$CODE" -ne 0 ]]; then
-  echo "I expected error!!!"
-else
-  echo "Every thing works as expected!!!"
-fi
+echo "I will run 1"
+
+./nawaphon.com 1
+
+echo "I will run 2 with 4 processes"
+
+./nawaphon.com 2 &
+PID_1=$!
+
+./nawaphon.com 2 &
+PID_2=$!
+
+./nawaphon.com 2 &
+PID_3=$!
+
+./nawaphon.com 2 &
+PID_4=$!
+
+wait "$PID_1" "$PID_2" "$PID_3" "$PID_4"
+
+echo "every thing done"
