189b086361e65e69a51c7cc9e1c9ba8e683fbb30
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-07-28 21:31:18 +0700

    Add concurrency_1 module with Run() function demonstrating file handling and delays

---
+++ concurrency_1/concuerrency_main_1.go
@@ -0,0 +1,28 @@
+package concurrency_1
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+func Run() {
+
+	file, err := os.OpenFile("./test.txt", os.O_WRONLY|os.O_CREATE, os.ModeExclusive)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("File opened!! next I will sleep for 10 seconds")
+
+	time.Sleep(10 * time.Second)
+
+	log.Println("I will exist the file should close.")
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+}
