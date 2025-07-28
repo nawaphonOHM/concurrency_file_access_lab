fb36365eef217cff7bb111b7b31aaf8bc03db2a6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-07-28 22:17:15 +0700

    Add parameter handling in `main.go` to switch between `concurrency_1` and `concurrency_2` execution paths

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"nawaphon.com/concurrency_1"
 	"nawaphon.com/concurrency_2"
+	"os"
 )
 
-func main() {
+func main_1() {
 	err := concurrency_1.Run()
 
 	if err != nil {
@@ -18,10 +19,31 @@ func main() {
 	if err == nil {
 		log.Printf("I expected error at this point.")
 	}
+}
 
-	err = concurrency_2.Run()
+func main_2() {
+	err := concurrency_2.Run()
 
 	if err != nil {
 		log.Fatal("I don't expected error at this point.")
+	}
+}
+
+func main() {
+
+	params := os.Args[1:]
+
+	if len(params) > 1 {
+		log.Fatal("I don't expected more than one parameter.")
+	}
+
+	if params[0] == "1" {
+		main_1()
+		return
+	} else if params[0] == "2" {
+		main_2()
+		return
+	} else {
+		log.Fatal("I expected only 1 or 2 as parameter.")
 	}
 }
